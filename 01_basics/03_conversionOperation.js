a82ee6ccea42b6b8e0d42adc808d7593a5d8f951
swatirawat2001 <rawatswati356@gmail.com>
2025-01-17 17:26:55 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = 33
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//convert 
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//null => 0
+//undefined => NaN
+
+
+let isLoggedIn = "swati"
+
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsloggedIn)
+console.log(booleanIsloggedIn)
+
+// 1 => true
+// "" => false
+// "swati" => true
+
+
+
+
+
+
