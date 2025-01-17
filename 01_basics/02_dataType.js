d957f41dd8b7471718392b4002d021de33882cd0
swatirawat2001 <rawatswati356@gmail.com>
2025-01-17 17:27:29 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as  newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+
+console.log(3 + 
+    3) //code readability should be high
+
+    //DATA TYPES
+
+    let name = "Swati Rawat"
+    let age = 23
+    let isLoggedIn = false
+
+    //two types of data 
+    //1. primitive data types and 2. non primitives data types
+    
+    //primitive data types
+    //numbers => 2 to power 53
+    //bigInt
+    //string => ""
+    //boolean => true/false
+    // null => standalone value
+    //undefined =>
+    // symbol => unique
+
+    //non primitive
+    //object
+
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); //object
+
+
